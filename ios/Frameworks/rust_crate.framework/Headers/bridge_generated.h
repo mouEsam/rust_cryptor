b1cac637ec2b9e0adec23cce39deac8df641c77a
Moustafa Essam <drsh_201122@yahoo.com>
2022-05-29 00:43:27 +0200

    add ios and macos

--- ios/Frameworks/rust_crate.framework/Headers/bridge_generated.h
+++ ios/Frameworks/rust_crate.framework/Headers/bridge_generated.h
@@ -2,6 +2,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct wire_uint_8_list {
+  uint8_t *ptr;
+  int32_t len;
+} wire_uint_8_list;
+
+typedef struct wire_CryptorHandle {
+  int64_t field0;
+} wire_CryptorHandle;
+
 typedef struct WireSyncReturnStruct {
   uint8_t *ptr;
   int32_t len;
@@ -14,6 +23,25 @@ typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
 
 void wire_greet(int64_t port_);
 
+void wire_cryptor_new(int64_t port_,
+                      int64_t sub_pool_id,
+                      struct wire_uint_8_list *key,
+                      uintptr_t iv_length);
+
+void wire_cryptor_encrypt(int64_t port_,
+                          struct wire_CryptorHandle *cryptor,
+                          struct wire_uint_8_list *text);
+
+void wire_cryptor_decrypt(int64_t port_,
+                          struct wire_CryptorHandle *cryptor,
+                          struct wire_uint_8_list *data);
+
+void wire_cryptor_remove(int64_t port_, struct wire_CryptorHandle *cryptor);
+
+struct wire_CryptorHandle *new_box_autoadd_cryptor_handle(void);
+
+struct wire_uint_8_list *new_uint_8_list(int32_t len);
+
 void free_WireSyncReturnStruct(struct WireSyncReturnStruct val);
 
 void store_dart_post_cobject(DartPostCObjectFnType ptr);
@@ -21,6 +49,12 @@ void store_dart_post_cobject(DartPostCObjectFnType ptr);
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
     dummy_var ^= ((int64_t) (void*) wire_greet);
+    dummy_var ^= ((int64_t) (void*) wire_cryptor_new);
+    dummy_var ^= ((int64_t) (void*) wire_cryptor_encrypt);
+    dummy_var ^= ((int64_t) (void*) wire_cryptor_decrypt);
+    dummy_var ^= ((int64_t) (void*) wire_cryptor_remove);
+    dummy_var ^= ((int64_t) (void*) new_box_autoadd_cryptor_handle);
+    dummy_var ^= ((int64_t) (void*) new_uint_8_list);
     dummy_var ^= ((int64_t) (void*) free_WireSyncReturnStruct);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
     return dummy_var;
--- ios/rust_cryptor.podspec
+++ ios/rust_cryptor.podspec
@@ -20,6 +20,7 @@ A new Flutter project.
   s.platform = :ios, '9.0'
 
   # Flutter.framework does not contain a i386 slice.
-  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
+  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
+  s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
   s.swift_version = '5.0'
 end
